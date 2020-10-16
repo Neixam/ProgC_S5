78faaade564ec0d2ff72b1614661de897a6a224f
Neixam <abourenn@student.42.fr>
2020-10-16 12:38:29 +0200

    (-) gmon compile (+) final cond of ex2

--- TP2/ex2.c
+++ TP2/ex2.c
@@ -5,20 +5,21 @@
 
 void	harmonics_series(long int n_iter, long int range)
 {
-	double     u; /* La valeur en float est ~ 2 097 000 */
-	long int   i;
+	double      u; /* La valeur en float est ~ 2 097 000 */
+    double      u_prv;
+	long int    i;
 
-	u = 1.0;
-	for (i = 2; i <= n_iter; i++)
+	for (i = 2, u = 1.0, u_prv = 0; i <= n_iter && u != u_prv; i++)
 	{
 		if (i % range == 0)
 			printf("%ld : %f\n", i, u);
+        u_prv = u;
 		u += 1.0 / ((double) i);
 	}
 }
 
 int		main(void)
 {
-	harmonics_series(100000000000, 1000);
+	harmonics_series(100000000000, 1000000);
 	return (0);
 }
